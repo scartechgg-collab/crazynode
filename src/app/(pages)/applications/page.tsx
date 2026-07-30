ba734df48f1c36f11851f7a3abdd12533cf92790
scartechgg-collab <scartechgg@gmail.com>
2026-07-30 11:27:40 +0530

    Delete src/app/(pages)/applications directory

--- src/app/(pages)/applications/page.tsx
+++
@@ -1,5 +0,0 @@
-import type { Metadata } from "next";
-import AboutClient from "./AboutClient";
-
-export const metadata: Metadata = { title: "Applications" };
-export default function AboutPage() { return <AboutClient />; }
